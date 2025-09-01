2cb40fe9dd15cb72dfe0204869429e6b31a4e2e5
link8430 <link8430@gmail.com>
2025-09-01 14:50:05 +0900

    9/1

--- ch09/src/main/java/dao/user1/User1DAO.java
+++ ch09/src/main/java/dao/user1/User1DAO.java
@@ -1,7 +1,7 @@
 package dao.user1;
 
-
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -14,38 +14,45 @@ import javax.sql.DataSource;
 
 import dto.user1.User1DTO;
 
-// DAO(Data Access Object) : DB처리를 수행하는 객체
+
+
+// DAO(Data Access Object) : DB처리를 수행하는 객체 
 public class User1DAO {
 	
-	// 싱글톤
-	private final static User1DAO INSTANCE = new User1DAO();
+	// DAO는 기본 싱글톤
+	private final static User1DAO INSTANCE = new User1DAO();	
 	public static User1DAO getInstance() {
 		return INSTANCE;
-	}
+	}	
 	private User1DAO() {}
 	
 	// 기본 CRUD 메서드
-	public void insertUser1(User1DTO dto) {		
+	public int insertUser1(User1DTO dto) {		
+		
+		int rowCount = 0;
+		
 		try {
 			Context ctx = (Context) new InitialContext().lookup("java:comp/env");
 			DataSource ds = (DataSource) ctx.lookup("jdbc/sundae517");
+			
 			Connection conn = ds.getConnection();
 			
-			String sql = "INSERT INTO USER1 VALUES(?,?,?,?)";
+			String sql = "INSERT INTO USER1 VALUES (?,?,?,?)";
 			PreparedStatement psmt = conn.prepareStatement(sql);
 			psmt.setString(1, dto.getUser_id());
 			psmt.setString(2, dto.getName());
 			psmt.setString(3, dto.getHp());
 			psmt.setInt(4, dto.getAge());
 			
-			psmt.executeUpdate();
+			// INSERT 성공하면 1, 실패하면 0
+			rowCount = psmt.executeUpdate();
 			
 			psmt.close();
 			conn.close();
-			
 		}catch (Exception e) {
-			e.printStackTrace();
-		}		
+			e.printStackTrace();			
+		}
+		return rowCount;
 	}
 	
 	public User1DTO selectUser1(String user_id) {
@@ -55,33 +62,31 @@ public class User1DAO {
 		try {
 			Context ctx = (Context) new InitialContext().lookup("java:comp/env");
 			DataSource ds = (DataSource) ctx.lookup("jdbc/sundae517");
+			
 			Connection conn = ds.getConnection();
 			
-			
-			String sql = "SELECT * FROM USER1 WHERE USER_ID = ?";
+			String sql = "SELECT * FROM USER1 WHERE user_id=?";
 			PreparedStatement psmt = conn.prepareStatement(sql);
 			psmt.setString(1, user_id);
 			
 			ResultSet rs = psmt.executeQuery();
 			
-			if(rs.next()) {
+			if(rs.next()) {				
 				dto = new User1DTO();
 				dto.setUser_id(rs.getString(1));
 				dto.setName(rs.getString(2));
 				dto.setHp(rs.getString(3));
-				dto.setAge(rs.getString(4));
-				
+				dto.setAge(rs.getInt(4));
 			}
 			
 			rs.close();
 			psmt.close();
 			conn.close();
-			
 		}catch (Exception e) {
-			e.printStackTrace();
+			e.printStackTrace();			
 		}
-		return dto;	
-
+		
+		return dto;
 	}
 	
 	public List<User1DTO> selectAllUser1() {
@@ -116,17 +121,18 @@ public class User1DAO {
 			e.printStackTrace();			
 		}	
 		
-		return dtoList;	
+		return dtoList;
 	}
 	
 	public void updateUser1(User1DTO dto) {
+		
 		try {
 			Context ctx = (Context) new InitialContext().lookup("java:comp/env");
 			DataSource ds = (DataSource) ctx.lookup("jdbc/sundae517");
+			
 			Connection conn = ds.getConnection();
-			
-			
-			String sql = "UPDATE USER1 SET NAME=?, HP=?, AGE=? WHERE USER_ID=?";
+						
+			String sql = "UPDATE USER1 SET name=?, hp=?, age=? WHERE user_id=?";
 			PreparedStatement psmt = conn.prepareStatement(sql);
 			psmt.setString(1, dto.getName());
 			psmt.setString(2, dto.getHp());
@@ -134,35 +140,32 @@ public class User1DAO {
 			psmt.setString(4, dto.getUser_id());
 			
 			psmt.executeUpdate();
-
+			
 			psmt.close();
 			conn.close();
-			
 		}catch (Exception e) {
-			e.printStackTrace();
-		}
-
+			e.printStackTrace();			
+		}	
+		
 	}
 	
 	public void deleteUser1(String user_id) {
 		try {
 			Context ctx = (Context) new InitialContext().lookup("java:comp/env");
 			DataSource ds = (DataSource) ctx.lookup("jdbc/sundae517");
+			
 			Connection conn = ds.getConnection();
-			
-			
-			String sql = "DELETE FROM USER1 WHERE USER_ID = ?";
+						
+			String sql = "DELETE FROM USER1 WHERE user_id=?";
 			PreparedStatement psmt = conn.prepareStatement(sql);
 			psmt.setString(1, user_id);
-					
+			
 			psmt.executeUpdate();
-
+			
 			psmt.close();
 			conn.close();
-			
 		}catch (Exception e) {
-			e.printStackTrace();
-		}
+			e.printStackTrace();			
+		}	
 	}
-	
 }
